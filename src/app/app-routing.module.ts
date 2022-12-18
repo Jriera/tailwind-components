99982761170aeaf92c4aa22e533f23179e0a0f90
Jordi Riera <jordiriera81@gmail.com>
2022-12-18 20:24:37 +0100

    chore:add routing for the component listing tailwind components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ComponentsListComponent} from "./components-list/components-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: ComponentsListComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
